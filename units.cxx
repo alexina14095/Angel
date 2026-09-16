8541ce44c35f401a64a0a768cf6797f2592d7f44
alexina14095 <alexinamakoye@gmail.com>
2026-09-16 08:07:35 +0300

    Add files via upload

---
+++ units.cxx
@@ -0,0 +1,18 @@
+//program to display academic units offered
+/*
+Author:Alexina
+Reg no:Bcs_05_0542/2026
+Description:program to display academic units offered
+Date:15/09/2026
+*/
+#include <stdio.h> // scanf (),print f ()
+int  main () { 
+printf("academic units.\n");
+printf("intoduction to programming.\n");
+printf("data structure.\n");
+printf("computer networks.\n");
+printf("database systems.\n");                                         
+printf("software engineering.\n");
+return 0 ;// execution successful
+}                                                     
+                         
